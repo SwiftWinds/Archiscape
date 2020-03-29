14d5601a9fb2272001a7955e9420515e2653694e
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-03-28 21:33:26 -0700

    Fix indentation

--- js/app.js
+++ js/app.js
@@ -35,13 +35,13 @@ angular.module('modelingApp', ['ngMaterial'])
                 img.onload = function() {
                     ctx.canvas.width = img.width;
                     ctx.canvas.height = img.height;
-				    ctx.drawImage(img, 0,0, img.width, img.height);
-				    //Retrieve data
-				    $scope.imgData = ctx.getImageData(0, 0, img.width, img.height);
-				    $scope.fileName = file.name;
-				    //Convert to binary format
-				    $scope.convertDataToBinaryArray($scope.imgData);
-				    $scope.$apply();
+								ctx.drawImage(img, 0,0, img.width, img.height);
+								//Retrieve data
+								$scope.imgData = ctx.getImageData(0, 0, img.width, img.height);
+								$scope.fileName = file.name;
+								//Convert to binary format
+								$scope.convertDataToBinaryArray($scope.imgData);
+								$scope.$apply();
                 }
                 img.src = URL.createObjectURL(file);
 
